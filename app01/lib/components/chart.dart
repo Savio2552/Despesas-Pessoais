ab4068d2dc28f48ab4c6e2487bb89864b422574d
savio2552 <saviogabryel2019@gmail.com>
2021-08-15 02:44:58 -0300

    App01

---
+++ app01/lib/components/chart.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import '../models/transaction.dart';
+import 'chart_bar.dart';
+
+class Chart extends StatelessWidget {
+  final List <Transaction> recentTransaction;
+
+  Chart (this.recentTransaction);
+
+  List<Map<String, Object>> get groupedTransactions{
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(
+        Duration(days:3),
+      );
+
+      double totalSum = 0.0;
+
+      for(var i = 0; i < recentTransaction.length; i++ ){
+       bool sameDay = recentTransaction[i].date.day == weekDay.day;
+       bool sameMonth = recentTransaction[i].date.month == weekDay.month;
+       bool sameYear = recentTransaction[i].date.year == weekDay.year;
+
+       if(sameDay && sameMonth && sameYear){
+         totalSum += recentTransaction[i].value;
+       }
+      }
+
+     
+
+      return {
+      'day': DateFormat.E().format(weekDay)[0],
+       'value':totalSum};
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      margin: EdgeInsets.all(20),
+      child: Row(
+          children: groupedTransactions.map((tr) {
+          return ChartBar(
+            label: tr['day'].toString(),
+            value: tr['value'] as double,
+            percentage: 0.3,
+          );
+        }).toList(),
+      ),
+      
+    );
+  }
+}
